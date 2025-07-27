c1d9a4a1276e4c8d3a6132ec13224c207aebd6a1
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-27 23:07:09 +0300

    Refactor Community and Discover routes to use centralized styles; update environment schema and add example .env file

--- src/app/(container)/(tabs)/community.tsx
+++ src/app/(container)/(tabs)/community.tsx
@@ -1,14 +1,21 @@
-
 import React from "react";
-import { Surface,Text } from "react-native-paper";
+import { Surface, Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-
+import { StyleSheet } from "react-native";
 
 export default function CommunityRoute() {
   const { top } = useSafeAreaInsets();
   return (
-    <Surface style={{ flex: 1, paddingTop: top + 12 }}>
-      <Text>Communuty Screen</Text>
+    <Surface style={[styles.container, { paddingTop: top + 12 }]}>
+      <Text>Community Screen</Text>
     </Surface>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- src/app/(container)/(tabs)/discover.tsx
+++ src/app/(container)/(tabs)/discover.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import {StyleSheet } from "react-native";
 import { Surface, Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function DiscoverRoute() {
   const { top } = useSafeAreaInsets();
   return (
-    <Surface style={{ flex: 1, paddingTop: top + 12 }}>
-    <Text>Discover Screen</Text>
+    <Surface style={[styles.container, { paddingTop: top + 12 }]}>
+      <Text>Discover Screen</Text>
     </Surface>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
